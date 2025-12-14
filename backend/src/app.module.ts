db229c8cdd8ed864469dbc4006b991a0a0de33d4
vita-tak <isacbroman@gmail.com>
2025-12-14 18:00:17 +0100

    add @nestjs/config for .env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProviderModule } from './provider/provider.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProviderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
