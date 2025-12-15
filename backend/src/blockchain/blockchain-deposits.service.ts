f9e71e223e32f0bfbc3aa62747bea66ca9451390
vita-tak <isacbroman@gmail.com>
2025-12-15 03:12:02 +0100

    implement DepositCreated event listener and processing

--- backend/src/blockchain/blockchain-deposits.service.ts
+++ backend/src/blockchain/blockchain-deposits.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { DepositStatus } from 'generated/prisma/client';
 
 @Injectable()
-export class BlockchainDepositsService {}
+export class BlockchainDepositsService {
+  private readonly logger = new Logger(BlockchainDepositsService.name);
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  async processDepositCreatedEvent(
+    depositId: bigint,
+    depositor: string,
+    beneficiary: string,
+    depositAmount: bigint,
+    periodStart: bigint,
+    periodEnd: bigint,
+    autoReleaseTime: bigint,
+    blockNumber: bigint,
+  ) {
+    this.logger.log(`Processing DepositCreated: ID ${depositId}`);
+
+    try {
+      await this.prisma.user.upsert({
+        where: {
+          walletAddress: depositor,
+        },
+        update: {},
+        create: {
+          walletAddress: depositor,
+        },
+      });
+
+      await this.prisma.user.upsert({
+        where: {
+          walletAddress: beneficiary,
+        },
+        update: {},
+        create: {
+          walletAddress: beneficiary,
+        },
+      });
+
+      await this.prisma.deposit.create({
+        data: {
+          onChainId: depositId.toString(),
+          depositorAddress: depositor,
+          beneficiaryAddress: beneficiary,
+          depositAmount: depositAmount.toString(),
+          periodStart: new Date(Number(periodStart) * 1000),
+          periodEnd: new Date(Number(periodEnd) * 1000),
+          autoReleaseTime: new Date(Number(autoReleaseTime) * 1000),
+          status: DepositStatus.WAITING_FOR_DEPOSIT,
+        },
+      });
+      this.logger.log(
+        `DepositCreated ${depositId} processed successfully (block: ${blockNumber})`,
+      );
+    } catch (error) {
+      this.logger.error(
+        `Failed to process DepositCreated ${depositId}:`,
+        error,
+      );
+      throw error;
+    }
+  }
+}
--- backend/src/blockchain/blockchain.service.ts
+++ backend/src/blockchain/blockchain.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { VIEM_PROVIDER, ViemProvider } from 'src/provider/provider.viem';
+import { DEPOSIT_ESCROW_ABI } from '../constants/contract';
+import { BlockchainDepositsService } from './blockchain-deposits.service';
+import { Address } from 'viem';
 
 @Injectable()
-export class BlockchainService {}
+export class BlockchainService implements OnModuleInit {
+  private readonly logger = new Logger(BlockchainService.name);
+  private readonly CONTRACT_ADDRESS: Address;
+
+  constructor(
+    @Inject(VIEM_PROVIDER) private readonly viem: ViemProvider,
+    private readonly configService: ConfigService,
+    private readonly blockchainDeposits: BlockchainDepositsService,
+  ) {
+    this.CONTRACT_ADDRESS = this.configService.getOrThrow('CONTRACT_ADDRESS');
+  }
+
+  onModuleInit() {
+    this.watchDepositEvents();
+  }
+
+  watchDepositEvents() {
+    this.logger.debug('Starting to watch Deposit events...');
+
+    this.viem.watchContractEvent({
+      abi: DEPOSIT_ESCROW_ABI,
+      address: this.CONTRACT_ADDRESS,
+      eventName: 'DepositCreated',
+      onLogs: (logs) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const log = logs[0] as any;
+
+        if (!log.args) return;
+
+        const {
+          depositId,
+          depositor,
+          beneficiary,
+          depositAmount,
+          periodStart,
+          periodEnd,
+          autoReleaseTime,
+        } = log.args;
+
+        void this.blockchainDeposits.processDepositCreatedEvent(
+          depositId,
+          depositor,
+          beneficiary,
+          depositAmount,
+          periodStart,
+          periodEnd,
+          autoReleaseTime,
+          log.blockNumber!,
+        );
+      },
+      onError: (error) => {
+        this.logger.error(`Error watching DepositCreated: ${error.message}`);
+      },
+    });
+  }
+}
