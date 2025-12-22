219828c3a3792785fe40360718fcb6b00af1040b
vita-tak <isacbroman@gmail.com>
2025-12-22 23:19:18 +0100

    add contract constants for DepositEscrow and USDC

---
+++ frontend/src/lib/contracts/usdc.ts
@@ -0,0 +1,32 @@
+export const usdcAddress =
+  '0x41E94Eb019C0762f9Bfcf9Fb1E58725BfB0e7582' as const;
+
+export const usdcAbi = [
+  {
+    type: 'function',
+    name: 'approve',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'spender', type: 'address' },
+      { name: 'amount', type: 'uint256' },
+    ],
+    outputs: [{ type: 'bool' }],
+  },
+  {
+    type: 'function',
+    name: 'allowance',
+    stateMutability: 'view',
+    inputs: [
+      { name: 'owner', type: 'address' },
+      { name: 'spender', type: 'address' },
+    ],
+    outputs: [{ type: 'uint256' }],
+  },
+  {
+    type: 'function',
+    name: 'balanceOf',
+    stateMutability: 'view',
+    inputs: [{ name: 'account', type: 'address' }],
+    outputs: [{ type: 'uint256' }],
+  },
+] as const;
