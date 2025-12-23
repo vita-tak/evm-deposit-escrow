6011902b3aed977c43d4f3db1568985349b422f1
vita-tak <isacbroman@gmail.com>
2025-12-23 03:07:34 +0100

    add Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+
+  console.log(`Backend running on: http://localhost:${port}`);
 }
 bootstrap();
